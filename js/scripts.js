490f53de0bd2e0230977aec79376b414943777dc
Kelsey Langlois and Megan Olson <megan@>
2017-08-17 11:48:12 -0700

    Add comments for clarification.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 var numerals = ["I", "V", "X", "L", "C", "D", "M"];
 var numeralValues = [1, 5, 10, 50, 100, 500, 1000];
 
+// We do not want empty inputs or negative numbers being converted
 function error(decimalNumber) {
   if (!Number.isInteger(decimalNumber)) {
     return "error: empty field";
@@ -12,23 +13,29 @@ function error(decimalNumber) {
 }
 
 function makeColumnRoman(decimalNumber) {
-  var numeralTotal = 0;
+  var numeralTotal = 0; // Keep track of total value of our Roman Numeral string so far.
   var result = "";
   for (var index = numeralValues.length - 1; index >= 0; index--) {
-    var numeralCount = 0;
+    // Go through our possible numeral values, starting with the biggest.
+    var numeralCount = 0; // We want to keep track of how many of a numeral we've added
     while (numeralTotal + numeralValues[index] <= decimalNumber) {
+      // If a numeral is small enough to add to the numeral string without going over our decimal ammount, we add it as many times as we can without going over the decimal ammount.
       numeralCount += 1;
       if (numeralCount > 3) {
+        // If we already have three numerals in a row we want to replace with the next greater numeral minus this numeral
         var greaterVal = numeralTotal + 2 * numeralValues[index];
         numeralTotal += (greaterVal - numeralValues[index]);
         var resultIndex = result.length - 1;
         while (numeralTotal > decimalNumber && resultIndex >= 0) {
+          // Remove all the numerals we'd already put in the string making the total too large, so we can add the new pair of numerals
           var thisNumeral = result.charAt(resultIndex);
           numeralTotal -= numeralValues[numerals.indexOf(thisNumeral)];
           resultIndex -= 1;
         }
+        // Make the new string now that we've removed the extra numerals
         result = result.substring(0, resultIndex) + numerals[index] + numerals[numeralValues.indexOf(greaterVal)];
       } else {
+        // Keep track of our total and add to string
         numeralTotal += numeralValues[index];
         result += numerals[index];
       }
@@ -39,15 +46,16 @@ function makeColumnRoman(decimalNumber) {
 
 function makeRoman(decimalNumber) {
   var numberError = error(decimalNumber);
-  if (numberError) {
+  if (numberError) { // Find out if we have an illegal input
     return numberError;
-  } else if (decimalNumber === 0) {
+  } else if (decimalNumber === 0) { // 0 doesn't need to be converted by column
     return "nulla";
-  } else {
+  } else { // We need to convert each column (10s, etc) individually
     var result = "";
-    var place = 10;
+    var place = 10; // What place (10s, 100s, etc) we're converting
     while (decimalNumber > 0) {
       result = makeColumnRoman(decimalNumber % place) + result;
+      // Since we go from smallest place to largest, add result to front of Roman Numeral string
       decimalNumber -= decimalNumber % place;
       place *= 10;
     }
