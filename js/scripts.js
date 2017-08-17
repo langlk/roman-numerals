38dd3f03afcef5d29f892e60d74d3fe66b91cdbe
Kelsey Langlois and Megan Olson <megan@>
2017-08-17 11:01:31 -0700

    Fix output for 4 and 9 cases.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // business logic
 
-var numerals = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"};
+var numerals = ["I", "V", "X", "L", "C", "D", "M"];
 var numeralValues = [1, 5, 10, 50, 100, 500, 1000];
 
 function error(decimalNumber) {
@@ -11,28 +11,56 @@ function error(decimalNumber) {
   }
 }
 
-function makeRoman(decimalNumber) {
-  var numberError = error(decimalNumber);
-  if (numberError) {
-    return numberError;
-  } else if (decimalNumber === 0) {
-    return "nulla";
-  } else if (numerals.hasOwnProperty(decimalNumber)) {
-    return numerals[decimalNumber];
+function makeColumnRoman(decimalNumber) {
+  if (numeralValues.includes(decimalNumber)) {
+    return numerals[numeralValues.indexOf(decimalNumber)];
   } else {
     var numeralTotal = 0;
     var result = "";
     for (var index = numeralValues.length - 1; index >= 0; index--) {
       var currentValue = numeralValues[index];
+      var numeralCount = 0;
       while (numeralTotal + currentValue <= decimalNumber) {
-        numeralTotal += currentValue;
-        result += numerals[currentValue];
+        numeralCount += 1;
+        if (numeralCount > 3) {
+          var greaterVal = numeralTotal + 2 * currentValue;
+          var greaterValIndex = numeralValues.indexOf(greaterVal);
+          numeralTotal += (greaterVal - currentValue);
+          var resultIndex = result.length - 1;
+          while (numeralTotal > decimalNumber && resultIndex >= 0) {
+            var thisNumeral = result.charAt(resultIndex);
+            numeralTotal -= numeralValues[numerals.indexOf(thisNumeral)];
+            resultIndex -= 1;
+          }
+          result = result.substring(0, resultIndex) + numerals[index] + numerals[greaterValIndex];
+        } else {
+          numeralTotal += currentValue;
+          result += numerals[index];
+        }
       }
     }
     return result;
   }
 }
 
+function makeRoman(decimalNumber) {
+  var numberError = error(decimalNumber);
+  if (numberError) {
+    return numberError;
+  } else if (decimalNumber === 0) {
+    return "nulla";
+  } else {
+    var result = "";
+    var place = 10;
+    while (decimalNumber > 0) {
+      var column = decimalNumber % place;
+      result = makeColumnRoman(column) + result;
+      decimalNumber -= column;
+      place *= 10;
+    }
+    return result;
+  }
+}
 
 // User Interface logic
 
