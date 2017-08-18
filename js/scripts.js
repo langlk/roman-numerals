1ba11db99143a5c215538bae3e032ad7506c148b
Kelsey Langlois <kels.langlois@gmail.com>
2017-08-18 13:46:40 -0700

    Refactor to simplify makeColumnRoman.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // business logic
 
-var numerals = ["I", "V", "X", "L", "C", "D", "M"];
-var numeralValues = [1, 5, 10, 50, 100, 500, 1000];
+var numerals = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
+var numeralValues = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
 
 // We do not want empty inputs or negative numbers being converted
 function error(decimalNumber) {
@@ -20,25 +20,8 @@ function makeColumnRoman(decimalNumber) {
     var numeralCount = 0; // We want to keep track of how many of a numeral we've added
     while (numeralTotal + numeralValues[index] <= decimalNumber) {
       // If a numeral is small enough to add to the numeral string without going over our decimal ammount, we add it as many times as we can without going over the decimal ammount.
-      numeralCount += 1;
-      if (numeralCount > 3) {
-        // If we already have three numerals in a row we want to replace with the next greater numeral minus this numeral
-        var greaterVal = numeralTotal + 2 * numeralValues[index];
-        numeralTotal += (greaterVal - numeralValues[index]);
-        var resultIndex = result.length - 1;
-        while (numeralTotal > decimalNumber && resultIndex >= 0) {
-          // Remove all the numerals we'd already put in the string making the total too large, so we can add the new pair of numerals
-          var thisNumeral = result.charAt(resultIndex);
-          numeralTotal -= numeralValues[numerals.indexOf(thisNumeral)];
-          resultIndex -= 1;
-        }
-        // Make the new string now that we've removed the extra numerals
-        result = result.substring(0, resultIndex) + numerals[index] + numerals[numeralValues.indexOf(greaterVal)];
-      } else {
-        // Keep track of our total and add to string
         numeralTotal += numeralValues[index];
         result += numerals[index];
-      }
     }
   }
   return result;
@@ -66,6 +49,8 @@ function makeRoman(decimalNumber) {
 // User Interface logic
 
 $(document).ready(function() {
+  console.log(numerals.length);
+  console.log(numeralValues.length);
   $("#number").submit(function(event) {
     event.preventDefault();
     var decimalNumber = parseInt($("input#decimal-number").val());
